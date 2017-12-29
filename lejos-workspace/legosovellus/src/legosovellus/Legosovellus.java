23defd982a958bef57f54788e596a125551231e9
Tommi Koivula <tommi.koivula@helsinki.fi>
2017-12-29 17:15:42 +0200

    ajamisen testailua

--- lejos-workspace/legosovellus/src/legosovellus/Legosovellus.java
+++ lejos-workspace/legosovellus/src/legosovellus/Legosovellus.java
@@ -1,6 +1,8 @@
 package legosovellus;
 
 import lejos.nxt.*;
+import lejos.robotics.RegulatedMotor;
+import lejos.util.Delay;
 import lejos.io.*;
 
 /**
@@ -10,9 +12,31 @@ import lejos.io.*;
 public class Legosovellus {
 
 	public static void main(String[] args) {
+		LCD.drawString("Testing", 0, 0);
+		aja(1080, true, 10000);
+		Delay.msDelay(1000);
+		aja(1080, false, 10000);
+		LCD.clearDisplay();
+		LCD.drawString("Press any button\nto exit", 0, 0);
+		Button.waitForPress();						
+	}
+	
+	
+	
+	public static void aja(int nopeus, boolean eteen, long aikaMillisekunteina) {
+		Motor.A.setSpeed(nopeus);
+		Motor.B.setSpeed(nopeus);
 		
-
-		LCD.drawString("Hello world", 1, 1);
-		Button.waitForPress();
+		if (eteen) {
+			Motor.A.forward();
+			Motor.B.backward();
+		} else {
+			Motor.A.backward();
+			Motor.B.forward();
+		}
+		
+		Delay.msDelay(aikaMillisekunteina);
+		Motor.A.stop();
+		Motor.B.stop();
 	}
 }
